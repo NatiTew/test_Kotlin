a706a37a459a69dc0e45e462bce29876fd73be29
Aepmong <tewter.chan@hotmail.com>
2020-10-11 13:49:28 +0700

    When Basic

---
+++ When.kt
@@ -0,0 +1,47 @@
+fun main(args: Array<String>) {
+    /* 
+    val a = 12
+    val b = 5
+
+    println("Enter operator either +, -, * or /")
+    val operator = readLine()
+
+    
+    val result = when (operator) {
+        "+" -> a + b
+        "-" -> a - b
+        "*" -> a * b
+        "/" -> a / b
+        "%" -> a % b
+        else -> "$operator operator is invalid operator."
+    }
+
+    println("result = $result")
+
+    when (operator) {
+        "+" -> println("$a + $b = ${a + b}")
+        "-" -> println("$a - $b = ${a - b}")
+        "*" -> println("$a * $b = ${a * b}")
+        "/" -> println("$a / $b = ${a / b}")
+        else -> println("$operator is invalid")
+    }
+
+    val n = -1
+
+    when (n) {
+        1, 2, 3 -> println("n is a positive integer less than 4.")
+        0 -> println("n is zero")
+        -1, -2 -> println("n is a negative integer greater than 3.")
+    }
+
+    val a = 100
+
+    when (a) {
+        in 1..10 -> println("A positive number less than 11.")
+        in 10..100 -> println("A positive number between 10 and 100 (inclusive)")
+    }
+    */
+
+   
+
+}
