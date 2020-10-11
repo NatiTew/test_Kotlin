b0638ab8d51620b07d08e749cd032df7f68aae0b
Aepmong <tewter.chan@hotmail.com>
2020-10-11 13:54:10 +0700

    When Basic

--- When_Kotlin.kt
+++ When_Kotlin.kt
@@ -33,14 +33,14 @@ fun main(args: Array<String>) {
         0 -> println("n is zero")
         -1, -2 -> println("n is a negative integer greater than 3.")
     }
-
+    */
     val a = 100
 
     when (a) {
         in 1..10 -> println("A positive number less than 11.")
         in 10..100 -> println("A positive number between 10 and 100 (inclusive)")
     }
-    */
+    
 
    
 
